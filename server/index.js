2420ccb3cafaac7d9985528329b3859c519ebaa6
Lester <LesterCuasay@live.co.uk>
2025-01-30 12:42:30 +0000

    feat - defined .env variables

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -10,6 +11,9 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 
 const CONNECTION_URL =
   `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@socialapp.1y5hs.mongodb.net/?retryWrites=true&w=majority&appName=SocialApp`;
