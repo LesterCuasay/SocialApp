ed147d27c1e02b674a38fd9be4151f69efe00134
Lester <LesterCuasay@live.co.uk>
2025-01-31 17:04:42 +0000

    feat - restarted client side and installed tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
-import React from "react";
 
-const App = () => {
-    return (
-        <div>
-            <h1>Hello!</h1>
-        </div>
-    )
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-300">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+    </div>
+  );
 }
 
 export default App;
