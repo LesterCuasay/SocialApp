6597f4d7ef86a29ccc669939af2bd585bcab6738
Lester <LesterCuasay@live.co.uk>
2025-02-24 12:22:21 +0000

    feat - added itienary for logged in users so they have to be logged in to see /feed

--- client/src/pages/feed/PostFeed.jsx
+++ client/src/pages/feed/PostFeed.jsx
@@ -1,15 +1,38 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Posts from "../posts/Posts";
 import PostCreateForm from "../posts/PostCreateForm";
+import { AuthContext } from "../../context/AuthContext";
 
 const PostFeed = () => {
+  const { user } = useContext(AuthContext);
   const [currentId, setCurrentId] = useState(null);
+
+  const isLoggedin = user !== null;
 
   return (
     <div>
-      <PostCreateForm currentId={currentId} setCurrentId={setCurrentId} />
-      
-      <Posts setCurrentId={setCurrentId} />
+      {isLoggedin ? (
+        <>
+          <PostCreateForm currentId={currentId} setCurrentId={setCurrentId} />
+
+          <Posts setCurrentId={setCurrentId} />
+        </>
+      ) : (
+        <div className="flex justify-center">
+          <div className="bg-white rounded p-8 flex flex-col">
+            <div className="mb-4">
+              <h2>
+                Have you got an account? if so <a href="/login">Login</a>
+              </h2>
+            </div>
+            <div>
+              <h3>
+                If not <a href="/signup">Sign up here!</a>
+              </h3>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
