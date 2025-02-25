4b0555eb5d44be6c79f2ed222bddf122891daade
Lester <LesterCuasay@live.co.uk>
2025-02-25 11:28:11 +0000

    feat - restyled layout, removed navbar added menu in sidebar + close icon

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,26 +1,12 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import NavBar from "./NavBar";
 import SideBar from "./SideBar";
 
 const Layout = () => {
   const [isSideBarOpen, setIsSideBarOpen] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth >= 768) {
-        setIsSideBarOpen(true);
-      } else {
-        setIsSideBarOpen(false);
-      }
-    };
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  
   return (
     <div>
-      <NavBar toggleSideBar={() => setIsSideBarOpen(!isSideBarOpen)} />
       <SideBar toggleSideBar={() => setIsSideBarOpen(!isSideBarOpen)} isOpen={isSideBarOpen} />
     </div>
   );
--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -3,6 +3,7 @@ import { FaHome } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import { RiUserAddLine } from "react-icons/ri";
+import { IoIosMenu, IoIosClose } from "react-icons/io";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
@@ -23,37 +24,44 @@ const SideBar = ({ isOpen, toggleSideBar }) => {
     toggleSideBar();
     handleLogout();
   };
- 
 
   return (
-    <div
-      className={`bg-gray-900 text-white w-full md:w-32 p-4 mt-16 shadow-md h-screen fixed left-0 transition-all ease-in-out duration-300
-        ${isOpen ? "translate-x-0" : "max-md:-translate-x-full"}
-        `}
-    >
-      {isLoggedin ? (
-        <>
-          <Link to="/feed" aria-label="Feed" onClick={toggleSideBar}>
-            <SideBarIcon icon={<FaHome size="28" />} text="Feed" />
-          </Link>
-          <Link to="/profile" aria-label="Profile" onClick={toggleSideBar}>
-            <SideBarIcon icon={<CgProfile size="28" />} text="Profile" />
-          </Link>
-          <Link aria-label="Logout" onClick={handleLogoutWithToggle}>
-            <SideBarIcon icon={<FaSignOutAlt size="28" />} text="Logout" />
-          </Link>
-        </>
-      ) : (
-        <>
-          <Link to="/signup" aria-label="Sign up" onClick={toggleSideBar}>
-            <SideBarIcon icon={<RiUserAddLine size="28" />} text="Sign Up" />
-          </Link>
-          <Link to="/login" aria-label="Login" onClick={toggleSideBar}>
-            <SideBarIcon icon={<FaSignInAlt size="28" />} text="Login" />
-          </Link>
-        </>
-      )}
-    </div>
+    <>
+      <button
+        onClick={toggleSideBar}
+        className={`md:hidden top-4 left-4 bg-gray-800 text-white p-2 rounded-4xl focus:outline-none absolute z-10 cursor-pointer
+        transition-transform duration-300 ${isOpen ? "rotate-180" : "" }`}
+      >
+        {isOpen ? <IoIosClose size={32} /> : <IoIosMenu size={32} />}
+      </button>
+      <div
+        className={`bg-gray-900 text-white w-full md:w-32 p-4 shadow-md h-screen fixed left-0 transition-all ease-in-out duration-300
+        ${isOpen ? "translate-x-0" : "max-md:-translate-x-full"}`}
+      >
+        {isLoggedin ? (
+          <>
+            <Link to="/feed" aria-label="Feed" onClick={toggleSideBar}>
+              <SideBarIcon icon={<FaHome size="28" />} text="Feed" />
+            </Link>
+            <Link to="/profile" aria-label="Profile" onClick={toggleSideBar}>
+              <SideBarIcon icon={<CgProfile size="28" />} text="Profile" />
+            </Link>
+            <Link aria-label="Logout" onClick={handleLogoutWithToggle}>
+              <SideBarIcon icon={<FaSignOutAlt size="28" />} text="Logout" />
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link to="/signup" aria-label="Sign up" onClick={toggleSideBar}>
+              <SideBarIcon icon={<RiUserAddLine size="28" />} text="Sign Up" />
+            </Link>
+            <Link to="/login" aria-label="Login" onClick={toggleSideBar}>
+              <SideBarIcon icon={<FaSignInAlt size="28" />} text="Login" />
+            </Link>
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
